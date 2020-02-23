cb646db1c8ee7c18cf052fb1857355abb71ecbc8
inkuringu-ika <59644555+inkuringu-ika@users.noreply.github.com>
2020-02-23 15:15:18 +0900

    記述の乱れを修正

--- soundcloud_dl.py
+++ soundcloud_dl.py
@@ -13,7 +13,7 @@ dir = os.path.dirname(sys.argv[0])
 #
 cprint('Warning: This program is pre-release and may be unstable.', 'yellow')
 print('Copyright (c) 2020 inkuringu-ika')
-print('This software is released under the GPL3.0 License, see LICENSE.')
+print('This software is released under the GPL3.0 License, see LICENSE file.')
 #
 
 

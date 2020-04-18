8c348fc363b2706fa0cb395fed7a789f0c990e6f
inkuringu-ika <59644555+inkuringu-ika@users.noreply.github.com>
2020-04-19 02:36:02 +0900

    Update soundcloud_dl.py
    2.0alpha

--- soundcloud_dl.py
+++ soundcloud_dl.py
@@ -1,12 +1,7 @@
-#from termcolor import colored, cprint
-#import colorama
-#colorama.init()
-
 from colorama import init, Fore, Style
 init()
 
 #
-#print(Fore.YELLOW + 'Warning: This program is pre-release and may be unstable.' + Style.RESET_ALL)
 print('Copyright (c) 2020 inkuringu-ika')
 print('This software is released under the GPL3.0 License, see LICENSE file.')
 #
@@ -20,24 +15,8 @@ import sys
 import traceback
 from tqdm import tqdm
 
-dir = os.path.dirname(sys.argv[0])
 
-
-
-
-#client_id
-
-#テスト用
-#client_id = '00000000000000000000000000000000'
-
-#有効期限がある
-#client_id = 'PANENeeumEFxeUKuTj575zguSBQI5DwE'
-
-#こっちのほうが有効期限長いかも?
 client_id = 'LBCcHmRB8XSStWL6wKH2HPACspQlXg2P'
-
-
-
 
 
 userinput = input('url>>')
@@ -47,7 +26,6 @@ if("soundcloud.com" in urllib.parse.urlparse(userinput).netloc):
 else:
     print(Fore.RED + 'Error: Url is wrong' + Style.RESET_ALL)
     sys.exit(1)
-
 try:
     request_url = userinput
     r = requests.get(request_url)
@@ -58,8 +36,17 @@ except:
 if(r.status_code == requests.codes.ok):
     pass
 else:
+    print("status_code:" + r.status_code)
     print(Fore.RED + 'Error: Url is wrong' + Style.RESET_ALL)
     sys.exit(1)
+
+try:
+    json_result = json.loads(re.search('e.data.forEach\(function\(e\){n\(e\)}\)}catch\(t\){}}\)},(.*)\);', r.text).group(1))[5]
+except:
+    print(Fore.RED + 'Error: Error' + Style.RESET_ALL)
+    traceback.print_exc()
+    sys.exit(1)
+
 #app_version
 try:
     #https://soundcloud.com/version.txt
@@ -69,64 +56,59 @@ try:
 except:
     print(Fore.RED + 'Error: Unexpected error' + Style.RESET_ALL)
     traceback.print_exc()
-    #app_version = '1587041896'
-    #print("app_version(native): " + app_version)
     sys.exit(1)
 
 
-#url_id to No.id
-request_url = 'https://soundcloud.com/oembed?format=json&url=' + userinput
-try:
-    r = requests.get(request_url)
-    Noid = re.search("tracks%2F(.*)&", json.loads(r.text)["html"]).group(1)
-    print("Track ID: " + Noid)
-except:
-    print(Fore.RED + 'Error: Url is wrong' + Style.RESET_ALL)
-    #print(Fore.RED + 'Error: Unexpected error' + Style.RESET_ALL)
-    #traceback.print_exc()
-    #print('Error: url is wrong')
+
+if(json_result["data"][0]["kind"] == "track"):
+    json_result_split = json_result["data"]
+elif(json_result["data"][0]["kind"] == "playlist"):
+    json_result_split = json_result["data"][0]["tracks"]
+else:
+    print(Fore.RED + 'Error: Error' + Style.RESET_ALL)
+    traceback.print_exc()
     sys.exit(1)
 
 
 try:
-    print("downloading...")
-    
-    request_url = 'https://api-v2.soundcloud.com/tracks?ids=' + Noid + '&client_id=' + client_id + "&app_version=" + app_version + "&app_locale=en"
-    r = requests.get(request_url)
-    json2 = json.loads(r.text)
-    if(json2[0]["downloadable"] and json2[0]["has_downloads_left"]):
-        request_url = "https://api-v2.soundcloud.com/tracks/" + Noid + '/download?client_id=' + client_id + "&app_version=" + app_version + "&app_locale=en"
-        r = requests.get(request_url)
-        request_url = json.loads(r.text)["redirectUri"]
-        res = requests.get(request_url,stream=True)
-        if(res.headers["content-type"] == "audio/x-wav"):
-            ctype = ".wav"
-        elif(res.headers["content-type"] == "audio/mpeg"):
-            ctype = ".mp3"
-        else:
-            print("format error")
-            raise Exception
-        pbar = tqdm(total=int(res.headers["content-length"]), unit="B", unit_scale=True)
-        with open(Noid + ctype, 'wb') as file:
-            for chunk in res.iter_content(chunk_size=1024):
-                file.write(chunk)
-                pbar.update(len(chunk))
-            pbar.close()
-    else:
-        print(Fore.YELLOW + 'Not a free download!' + Style.RESET_ALL)
-        #print('not free download')
-        request_url = json2[0]["media"]["transcodings"][1]["url"] + '?client_id=' + client_id
-        r = requests.get(request_url)
-        json3 = json.loads(r.text)
-        url = json3["url"]
-        request_url = url
-        res = requests.get(request_url,stream=True)
-        pbar = tqdm(total=int(res.headers["content-length"]), unit="B", unit_scale=True)
-        with open(Noid + '.mp3', 'wb') as file:
-            for chunk in res.iter_content(chunk_size=1024):
-                file.write(chunk)
-                pbar.update(len(chunk))
-            pbar.close()
+    for json_result_split_onedata in json_result_split:
+        try:
+            print("downloading...")
+            Noid = str(json_result_split_onedata["id"])
+            print("Track ID: " + Noid)
+            request_url = 'https://api-v2.soundcloud.com/tracks?ids=' + Noid + '&client_id=' + client_id + "&app_version=" + app_version + "&app_locale=en"
+            r = requests.get(request_url)
+            json2 = json.loads(r.text)
+            if(json2[0]["downloadable"] and json2[0]["has_downloads_left"]):
+                request_url = "https://api-v2.soundcloud.com/tracks/" + Noid + '/download?client_id=' + client_id + "&app_version=" + app_version + "&app_locale=en"
+                r = requests.get(request_url)
+                request_url = json.loads(r.text)["redirectUri"]
+                res = requests.get(request_url,stream=True)
+                filename = res.headers["content-disposition"][res.headers["content-disposition"].find("filename=") + len("filename="):].replace('"',"")
+                pbar = tqdm(total=int(res.headers["content-length"]), unit="B", unit_scale=True)
+                with open(filename, 'wb') as file:
+                    for chunk in res.iter_content(chunk_size=1024):
+                        file.write(chunk)
+                        pbar.update(len(chunk))
+                    pbar.close()
+            else:
+                print(Fore.YELLOW + 'Not a free download!' + Style.RESET_ALL)
+                #print('not free download')
+                filename = re.sub(r'[\\|/|:|\*|?|"|<|>|\|]',"_",json2[0]["title"] + ".mp3")
+                request_url = json2[0]["media"]["transcodings"][1]["url"] + '?client_id=' + client_id
+                r = requests.get(request_url)
+                url = json.loads(r.text)["url"]
+                request_url = url
+                res = requests.get(request_url,stream=True)
+                pbar = tqdm(total=int(res.headers["content-length"]), unit="B", unit_scale=True)
+                with open(filename, 'wb') as file:
+                    for chunk in res.iter_content(chunk_size=1024):
+                        file.write(chunk)
+                        pbar.update(len(chunk))
+                    pbar.close()
+        except:
+            print(Fore.RED + 'Error: Unexpected error' + Style.RESET_ALL)
+            traceback.print_exc()
 except:
     print(Fore.RED + 'Error: Unexpected error' + Style.RESET_ALL)
     traceback.print_exc()

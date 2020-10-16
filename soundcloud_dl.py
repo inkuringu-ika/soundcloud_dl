26968c3c260046a8ef71c381d8e18b34431125fb
inkuringu-ika <59644555+inkuringu-ika@users.noreply.github.com>
2020-10-17 04:32:55 +0900

    Update files

--- soundcloud_dl.py
+++ soundcloud_dl.py
@@ -6,6 +6,7 @@ import os
 import urllib
 import re
 import traceback
+from bs4 import BeautifulSoup
 from tqdm import tqdm
 from colorama import init, Fore, Style
 init()
@@ -18,7 +19,13 @@ print('Copyright (c) 2020 inkuringu-ika')
 print('This software is released under the "GNU GENERAL PUBLIC LICENSE Version 3", see LICENSE file.')
 print()
 
-native_version = "6.1.1"
+native_version = "6.3.0"
+if(program_directory_path == os.getcwd()):
+    if(not os.path.exists("./downloads/")):
+        os.mkdir("./downloads")
+    save_directory = "./downloads"
+else:
+    save_directory = "."
 requests_option = {
     'Accept':'*/*',
     'Accept-Encoding':'gzip, deflate, br',
@@ -33,13 +40,38 @@ def client_id():
         'Accept-Language':'ja-JP,ja;q=0.9,en-US;q=0.8,en;q=0.7',
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
     }
-    request_url = "https://inkuringu-ika.github.io/api/soundcloud-client_id.json"
-    r = requests.get(request_url, headers=requests_option, timeout=10)
-    status_code = r.status_code
-    r.raise_for_status()
-    for for_json in json.loads(r.text):
-        if(for_json["type"] == "pc_browser"):
-            return for_json["client_id"]
+    try:
+        print("Fetching latest client_id...")
+        request_url = "https://soundcloud.com/"
+        r = requests.get(request_url, headers=requests_option, timeout=10)
+        status_code = r.status_code
+        r.raise_for_status()
+        soup = BeautifulSoup(r.text, "html.parser")
+        request_url = soup.find_all("script")[-2].get("src")
+        r = requests.get(request_url, headers=requests_option, timeout=10)
+        status_code = r.status_code
+        r.raise_for_status()
+        for for_data in r.text.split("{")[3].split("}")[0].split(","):
+            if(for_data.split(":")[0] == "client_id"):
+                #print(for_data.split(":")[1][1:][:-1])
+                #break
+                #testcode raise Exception
+                return for_data.split(":")[1][1:][:-1]
+        print(Fore.RED + "No Data" + Style.RESET_ALL)
+        raise Exception
+    except KeyboardInterrupt:
+        raise KeyboardInterrupt
+    except:
+        traceback.print_exc()
+        print(Fore.YELLOW + "Failed to get the latest client_id. Fetching old client_id..." + Style.RESET_ALL)
+        #github page
+        request_url = "https://inkuringu-ika.github.io/api/soundcloud-client_id.json"
+        r = requests.get(request_url, headers=requests_option, timeout=10)
+        status_code = r.status_code
+        r.raise_for_status()
+        for for_json in json.loads(r.text):
+            if(for_json["type"] == "pc_browser"):
+                return for_json["client_id"]
 
 def app_version():
     requests_option = {
@@ -68,7 +100,7 @@ def get_info(inputurl):
     r.raise_for_status()
     return json.loads(r.text)
 
-def download_track(track_info):
+def download_track(track_info, save_directory):
     requests_option = {
         'Accept':'*/*',
         'Accept-Encoding':'gzip, deflate, br',
@@ -100,7 +132,7 @@ def download_track(track_info):
         r.raise_for_status()
         filename = re.sub(r'[\\|/|:|\*|?|"|<|>|\||\n]',"_",track_info_2["title"] + "." + r.headers["content-disposition"][r.headers["content-disposition"].find("filename=") + len("filename="):].replace('"',"").split(".")[-1])
         pbar = tqdm(total=int(r.headers["content-length"]), unit="B", unit_scale=True)
-        with open(filename, 'wb') as file:
+        with open(save_directory + "/" + filename, 'wb') as file:
             for chunk in r.iter_content(chunk_size=1024):
                 file.write(chunk)
                 pbar.update(len(chunk))
@@ -129,7 +161,7 @@ def download_track(track_info):
             status_code = r.status_code
             r.raise_for_status()
             pbar = tqdm(total=int(r.headers["content-length"]), unit="B", unit_scale=True)
-            with open(filename, 'wb') as file:
+            with open(save_directory + "/" + filename, 'wb') as file:
                 for chunk in r.iter_content(chunk_size=1024):
                     file.write(chunk)
                     pbar.update(len(chunk))
@@ -143,12 +175,12 @@ def download_track(track_info):
             ffmpeg_path = program_directory_path + "\\ffmpeg.exe"
             if(os.path.isfile(ffmpeg_path)):
                 print("ffmpeg_path: " + ffmpeg_path)
-                subprocess.call([ffmpeg_path,"-y","-i",request_url,"-c","copy",filename])
+                subprocess.call([ffmpeg_path,"-y","-i",request_url,"-c","copy",save_directory + "/" + filename])
             else:
                 #print(Fore.YELLOW + "FFmpeg is required to download this audio." + Style.RESET_ALL)
                 print(Fore.YELLOW + "Download using the built-in hls downloader. Use ffmpeg if it does not download properly." + Style.RESET_ALL)
                 r = requests.get(request_url)
-                file = open(filename,mode="wb")
+                file = open(save_directory + "/" + filename,mode="wb")
                 for for_data in r.text.split("\n"):
                     if(not for_data[0] == "#"):
                         print("Downloading segment...")
@@ -236,8 +268,8 @@ except requests.exceptions.ConnectTimeout:
     traceback.print_exc()
     sys.exit(1)
 
-print("app_version: " + app_version)
 print("client_id: " + client_id)
+print("app_version: " + app_version)
 
 if(json_result["kind"] == "track"):
     track_list = [json_result]
@@ -290,9 +322,10 @@ else:
     print(Fore.YELLOW + 'Error: Unsupported url' + Style.RESET_ALL)
     sys.exit(1)
 
+
 for for_json in track_list:
     try:
-        download_track(for_json)
+        download_track(for_json, save_directory)
     except requests.exceptions.ConnectionError:
         print(Fore.RED + 'Error: network error.' + Style.RESET_ALL)
         traceback.print_exc()
@@ -302,5 +335,3 @@ for for_json in track_list:
     except requests.exceptions.ConnectTimeout:
         print(Fore.RED + 'Error: Timed out. The SoundCloud server may be down.' + Style.RESET_ALL)
         traceback.print_exc()
-
-#client_id = 'LBCcHmRB8XSStWL6wKH2HPACspQlXg2P'
